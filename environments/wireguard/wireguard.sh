75e683a19629a4a92a7dcaa12317f6dd9ed1aaab
Markus Heiliger <markus.heiliger@microsoft.com>
2021-02-19 13:25:55 -0800

    ...

--- environments/wireguard/wireguard.sh
+++ environments/wireguard/wireguard.sh
@@ -1,306 +1,47 @@
 #!/bin/bash
 
-# ## unattended-upgrade
-# apt-get update -y 
-# unattended-upgrades --verbose
+## init transkript
+exec &> >(tee -a "${0%.*}.log")
 
-# ## IP Forwarding
-# sed -i -e 's/#net.ipv4.ip_forward.*/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-# sed -i -e 's/#net.ipv6.conf.all.forwarding.*/net.ipv6.conf.all.forwarding=1/g' /etc/sysctl.conf
-# sysctl -p
+## upgrade packages
+apt-get update -y && unattended-upgrades --verbose
 
-# ## Install WireGurard
-# add-apt-repository ppa:wireguard/wireguard -y 
-# apt-get update -y 
-# apt-get install linux-headers-$(uname -r) -y
-# apt-get install wireguard -y
+## enable IP forwarding
+sed -i -e 's/#net.ipv4.ip_forward.*/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+sed -i -e 's/#net.ipv6.conf.all.forwarding.*/net.ipv6.conf.all.forwarding=1/g' /etc/sysctl.conf
+sysctl -p
 
-# ## Configure WireGuard
+## install WireGurard
+add-apt-repository ppa:wireguard/wireguard -y && apt-get update -y 
+apt-get install linux-headers-$(uname -r) wireguard -y
 
-# # Generate security keys
-# mkdir /home/$2/WireGuardSecurityKeys
-# umask 077
-# wg genkey | tee /home/$2/WireGuardSecurityKeys/server_private_key | wg pubkey > /home/$2/WireGuardSecurityKeys/server_public_key
-# wg genpsk > /home/$2/WireGuardSecurityKeys/preshared_key
-# wg genkey | tee /home/$2/WireGuardSecurityKeys/client_one_private_key | wg pubkey > /home/$2/WireGuardSecurityKeys/client_one_public_key
-# wg genkey | tee /home/$2/WireGuardSecurityKeys/client_two_private_key | wg pubkey > /home/$2/WireGuardSecurityKeys/client_two_public_key
-# wg genkey | tee /home/$2/WireGuardSecurityKeys/client_three_private_key | wg pubkey > /home/$2/WireGuardSecurityKeys/client_three_public_key
-# wg genkey | tee /home/$2/WireGuardSecurityKeys/client_four_private_key | wg pubkey > /home/$2/WireGuardSecurityKeys/client_four_public_key
-# wg genkey | tee /home/$2/WireGuardSecurityKeys/client_five_private_key | wg pubkey > /home/$2/WireGuardSecurityKeys/client_five_public_key
-# wg genkey | tee /home/$2/WireGuardSecurityKeys/client_six_private_key | wg pubkey > /home/$2/WireGuardSecurityKeys/client_six_public_key
-# wg genkey | tee /home/$2/WireGuardSecurityKeys/client_seven_private_key | wg pubkey > /home/$2/WireGuardSecurityKeys/client_seven_public_key
-# wg genkey | tee /home/$2/WireGuardSecurityKeys/client_eight_private_key | wg pubkey > /home/$2/WireGuardSecurityKeys/client_eight_public_key
-# wg genkey | tee /home/$2/WireGuardSecurityKeys/client_nine_private_key | wg pubkey > /home/$2/WireGuardSecurityKeys/client_nine_public_key
-# wg genkey | tee /home/$2/WireGuardSecurityKeys/client_ten_private_key | wg pubkey > /home/$2/WireGuardSecurityKeys/client_ten_public_key
+## generate security keys
+wg genkey | tee /etc/wireguard/privatekey | wg pubkey > /etc/wireguard/publickey
 
-# # Generate configuration files
-# server_private_key=$(</home/$2/WireGuardSecurityKeys/server_private_key)
-# preshared_key=$(</home/$2/WireGuardSecurityKeys/preshared_key)
-# server_public_key=$(</home/$2/WireGuardSecurityKeys/server_public_key)
-# client_one_private_key=$(</home/$2/WireGuardSecurityKeys/client_one_private_key)
-# client_one_public_key=$(</home/$2/WireGuardSecurityKeys/client_one_public_key)
-# client_two_private_key=$(</home/$2/WireGuardSecurityKeys/client_two_private_key)
-# client_two_public_key=$(</home/$2/WireGuardSecurityKeys/client_two_public_key)
-# client_three_private_key=$(</home/$2/WireGuardSecurityKeys/client_three_private_key)
-# client_three_public_key=$(</home/$2/WireGuardSecurityKeys/client_three_public_key)
-# client_four_private_key=$(</home/$2/WireGuardSecurityKeys/client_four_private_key)
-# client_four_public_key=$(</home/$2/WireGuardSecurityKeys/client_four_public_key)
-# client_five_private_key=$(</home/$2/WireGuardSecurityKeys/client_five_private_key)
-# client_five_public_key=$(</home/$2/WireGuardSecurityKeys/client_five_public_key)
-# client_six_private_key=$(</home/$2/WireGuardSecurityKeys/client_six_private_key)
-# client_six_public_key=$(</home/$2/WireGuardSecurityKeys/client_six_public_key)
-# client_seven_private_key=$(</home/$2/WireGuardSecurityKeys/client_seven_private_key)
-# client_seven_public_key=$(</home/$2/WireGuardSecurityKeys/client_seven_public_key)
-# client_eight_private_key=$(</home/$2/WireGuardSecurityKeys/client_eight_private_key)
-# client_eight_public_key=$(</home/$2/WireGuardSecurityKeys/client_eight_public_key)
-# client_nine_private_key=$(</home/$2/WireGuardSecurityKeys/client_nine_private_key)
-# client_nine_public_key=$(</home/$2/WireGuardSecurityKeys/client_nine_public_key)
-# client_ten_private_key=$(</home/$2/WireGuardSecurityKeys/client_ten_private_key)
-# client_ten_public_key=$(</home/$2/WireGuardSecurityKeys/client_ten_public_key)
+## create server config
+cat > /etc/wireguard/wg0.conf << EOF
 
-# # Server Config
-# cat > /etc/wireguard/wg0.conf << EOF
-# [Interface]
-# Address = 10.13.13.1/24
-# SaveConfig = true
-# PrivateKey = $server_private_key
-# ListenPort = 51820
-# PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE; ip6tables -A FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-# PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE; ip6tables -D FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+[Interface]
+Address = 192.168.250.1/24
+SaveConfig = true
+PrivateKey = $(cat /etc/wireguard/privatekey)
+ListenPort = 51820
+PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE; ip6tables -A FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE; ip6tables -D FORWARD -i wg0 -j ACCEPT; ip6tables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
 
-# [Peer]
-# PublicKey =  $client_one_public_key
-# PresharedKey = $preshared_key
-# AllowedIps = 10.13.13.101/32
+EOF
 
-# [Peer]
-# PublicKey =  $client_two_public_key
-# PresharedKey = $preshared_key
-# AllowedIps = 10.13.13.102/32
+## make server config accessible
+chmod 600 /etc/wireguard/{privatekey,wg0.conf}
 
-# [Peer]
-# PublicKey =  $client_three_public_key
-# PresharedKey = $preshared_key
-# AllowedIps = 10.13.13.103/32
+## configure firewall 
+ufw allow 51820/udp
+ufw allow 22/tcp
+ufw enable
 
-# [Peer]
-# PublicKey =  $client_four_public_key
-# PresharedKey = $preshared_key
-# AllowedIps = 10.13.13.104/32
+## start WireGuard service
+wg-quick up wg0
+systemctl enable wg-quick@wg0
 
-# [Peer]
-# PublicKey =  $client_five_public_key
-# PresharedKey = $preshared_key
-# AllowedIps = 10.13.13.105/32
-
-# [Peer]
-# PublicKey =  $client_six_public_key
-# PresharedKey = $preshared_key
-# AllowedIps = 10.13.13.106/32
-
-# [Peer]
-# PublicKey =  $client_seven_public_key
-# PresharedKey = $preshared_key
-# AllowedIps = 10.13.13.107/32
-
-# [Peer]
-# PublicKey =  $client_eight_public_key
-# PresharedKey = $preshared_key
-# AllowedIps = 10.13.13.108/32
-
-# [Peer]
-# PublicKey =  $client_nine_public_key
-# PresharedKey = $preshared_key
-# AllowedIps = 10.13.13.109/32
-
-# [Peer]
-# PublicKey =  $client_ten_public_key
-# PresharedKey = $preshared_key
-# AllowedIps = 10.13.13.110/32
-# EOF
-
-# # Client Configs
-# cat > /home/$2/wg0-client-1.conf << EOF
-# [Interface]
-# PrivateKey = $client_one_private_key
-# Address = 10.13.13.101/32
-# DNS = 1.1.1.1
-
-# [Peer]
-# PublicKey =  $server_public_key
-# PresharedKey = $preshared_key
-# EndPoint = $1:51820
-# AllowedIps = 0.0.0.0/0, ::/0
-# PersistentKeepAlive = 25
-
-# EOF
-
-# chmod go+r /home/$2/wg0-client-1.conf
-
-# cat > /home/$2/wg0-client-2.conf << EOF
-# [Interface]
-# PrivateKey = $client_two_private_key
-# Address = 10.13.13.102/32
-# DNS = 1.1.1.1
-
-# [Peer]
-# PublicKey =  $server_public_key
-# PresharedKey = $preshared_key
-# EndPoint = $1:51820
-# AllowedIps = 0.0.0.0/0, ::/0
-# PersistentKeepAlive = 25
-
-# EOF
-
-# chmod go+r /home/$2/wg0-client-2.conf
-
-# cat > /home/$2/wg0-client-3.conf << EOF
-# [Interface]
-# PrivateKey = $client_three_private_key
-# Address = 10.13.13.103/32
-# DNS = 1.1.1.1
-
-# [Peer]
-# PublicKey =  $server_public_key
-# PresharedKey = $preshared_key
-# EndPoint = $1:51820
-# AllowedIps = 0.0.0.0/0, ::/0
-# PersistentKeepAlive = 25
-
-# EOF
-
-# chmod go+r /home/$2/wg0-client-3.conf
-
-# cat > /home/$2/wg0-client-4.conf << EOF
-# [Interface]
-# PrivateKey = $client_four_private_key
-# Address = 10.13.13.104/32
-# DNS = 1.1.1.1
-
-# [Peer]
-# PublicKey =  $server_public_key
-# PresharedKey = $preshared_key
-# EndPoint = $1:51820
-# AllowedIps = 0.0.0.0/0, ::/0
-# PersistentKeepAlive = 25
-
-# EOF
-
-# chmod go+r /home/$2/wg0-client-4.conf
-
-# cat > /home/$2/wg0-client-5.conf << EOF
-# [Interface]
-# PrivateKey = $client_five_private_key
-# Address = 10.13.13.105/32
-# DNS = 1.1.1.1
-
-# [Peer]
-# PublicKey =  $server_public_key
-# PresharedKey = $preshared_key
-# EndPoint = $1:51820
-# AllowedIps = 0.0.0.0/0, ::/0
-# PersistentKeepAlive = 25
-
-# EOF
-
-# chmod go+r /home/$2/wg0-client-5.conf
-
-# cat > /home/$2/wg0-client-6.conf << EOF
-# [Interface]
-# PrivateKey = $client_six_private_key
-# Address = 10.13.13.106/32
-# DNS = 1.1.1.1
-
-# [Peer]
-# PublicKey =  $server_public_key
-# PresharedKey = $preshared_key
-# EndPoint = $1:51820
-# AllowedIps = 0.0.0.0/0, ::/0
-# PersistentKeepAlive = 25
-
-# EOF
-
-# chmod go+r /home/$2/wg0-client-6.conf
-
-# cat > /home/$2/wg0-client-7.conf << EOF
-# [Interface]
-# PrivateKey = $client_seven_private_key
-# Address = 10.13.13.107/32
-# DNS = 1.1.1.1
-
-# [Peer]
-# PublicKey =  $server_public_key
-# PresharedKey = $preshared_key
-# EndPoint = $1:51820
-# AllowedIps = 0.0.0.0/0, ::/0
-# PersistentKeepAlive = 25
-
-# EOF
-
-# chmod go+r /home/$2/wg0-client-7.conf
-
-# cat > /home/$2/wg0-client-8.conf << EOF
-# [Interface]
-# PrivateKey = $client_eight_private_key
-# Address = 10.13.13.108/32
-# DNS = 1.1.1.1
-
-# [Peer]
-# PublicKey =  $server_public_key
-# PresharedKey = $preshared_key
-# EndPoint = $1:51820
-# AllowedIps = 0.0.0.0/0, ::/0
-# PersistentKeepAlive = 25
-
-# EOF
-
-# chmod go+r /home/$2/wg0-client-8.conf
-
-# cat > /home/$2/wg0-client-9.conf << EOF
-# [Interface]
-# PrivateKey = $client_nine_private_key
-# Address = 10.13.13.109/32
-# DNS = 1.1.1.1
-
-# [Peer]
-# PublicKey =  $server_public_key
-# PresharedKey = $preshared_key
-# EndPoint = $1:51820
-# AllowedIps = 0.0.0.0/0, ::/0
-# PersistentKeepAlive = 25
-
-# EOF
-
-# chmod go+r /home/$2/wg0-client-9.conf
-
-# cat > /home/$2/wg0-client-10.conf << EOF
-# [Interface]
-# PrivateKey = $client_ten_private_key
-# Address = 10.13.13.110/32
-# DNS = 1.1.1.1
-
-# [Peer]
-# PublicKey =  $server_public_key
-# PresharedKey = $preshared_key
-# EndPoint = $1:51820
-# AllowedIps = 0.0.0.0/0, ::/0
-# PersistentKeepAlive = 25
-
-# EOF
-
-# chmod go+r /home/$2/wg0-client-10.conf
-
-# ## Firewall 
-# ufw allow 51820/udp
-# ufw allow 22/tcp
-# ufw enable
-
-# ## WireGuard Service
-# wg-quick up wg0
-# systemctl enable wg-quick@wg0
-
-# ## Upgrade
-# apt-get full-upgrade -y
-
-# ## Shutdown 
-# shutdown -r 1440
+## system upgrade
+apt-get full-upgrade -y
